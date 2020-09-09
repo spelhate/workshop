3c96f36d555bb615740f8dee7f649e6ef6a2ebb3
HANNAH FORD <22096@CR-BRETAGNE.FR>
2020-09-09 10:24:32 +0200

    fixes #1

--- wks01/script.js
+++ wks01/script.js
@@ -1,5 +1,12 @@
-function ShowCitySelected(selTag) {
+/*function ShowCitySelected() {
     var selectCity = document.getElementById("CitySelector");
     var selectedOption = selectCity.options[selectCity.selectedIndex].text;
-    document.getElementById("output").innerHTML = "Vous avez choisi " + selectedOption;
-  }
+    var region = selectCity.options[selectCity.selectedIndex].getAttribute("data-infos");
+    document.getElementById("output").innerHTML = "Vous avez choisi " + selectedOption + " qui se situe en " + region + ".";
+  }*/
+  
+function ShowCitySelected(selTag) {
+  var region = selTag.options[selTag.selectedIndex].getAttribute("data-infos");
+  document.getElementById("output").innerHTML = "Vous avez choisi " + selTag.options[selTag.selectedIndex].textContent + " qui se situe en " + region + ".";
+}
+
