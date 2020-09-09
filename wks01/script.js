1ca1f9184eccf4964e2918480006b2062553389f
HANNAH FORD <22096@CR-BRETAGNE.FR>
2020-09-09 10:33:13 +0200

    Uses data.json to fill select list

--- wks01/script.js
+++ wks01/script.js
@@ -10,3 +10,33 @@ function ShowCitySelected(selTag) {
   document.getElementById("output").innerHTML = "Vous avez choisi " + selTag.options[selTag.selectedIndex].textContent + " qui se situe en " + region + ".";
 }
 
+var select = document.getElementById('CitySelector');
+select.length = 0;
+
+var defaultText = document.createElement('option');
+defaultText.textContent = "Sélectionner une ville"
+
+select.add(defaultText);
+select.selectedIndex = 0;
+
+var url = "data.json";
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', url, true);
+
+xhr.onload = function(){
+  if (xhr.status === 200){
+    const data = JSON.parse(xhr.responseText);
+    var selectOption;
+    for ( let i = 0; i < data.length; i++){
+      selectOption = document.createElement('option');
+      selectOption.text = data[i].city;
+      selectOption.value = data[i].id;
+      selectOption.setAttribute("data-infos", data[i].infos);
+      select.add(selectOption);
+    }
+  }else{}
+};
+
+xhr.send();
+
